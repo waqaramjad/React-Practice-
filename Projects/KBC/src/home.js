db839240d683aa4995bfac204965cb9b293af8a6
waqaramjad <waqaramjad345@gmail.com>
2017-12-16 12:04:43 +0500

    Creating Project Kon banega crore pati

---
+++ Projects/KBC/src/home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class home extends Component
+{
+
+
+createNew(){
+
+}
+
+
+render(){
+
+    return(
+<div>
+<button onClick={this.createNew}>Create New Question</button>
+{/* <button onClick={}>Login</button> */}
+
+</div>
+
+
+    )
+}
+
+
+
+}
+
+export default home 
---
+++ Projects/KBC/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Counterp from './CounterP'
+import App from './App';
+import Func from './Route.js'
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Func/>, document.getElementById('root'));
+// ReactDOM.render(<App/>, document.getElementById('root'));
+// registerServiceWorker();
